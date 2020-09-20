62dabd4be4371044f66d286cef6432a730e6955b
kesharaJayasinghe <kesharalahiru1@gmail.com>
2020-09-20 19:57:43 +0530

    inital commit

---
+++ android/app/src/main/kotlin/com/keshjay/shopapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.keshjay.shopapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shop App'),
+      ),
+      body: Center(
+        child: Text('Welcome to Shop App!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.ac_unit),
+      ),
+    );
+  }
+}
